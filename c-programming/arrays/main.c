f303528ac2ea36aab23e318433bf32de8f22b2eb
MUEGHE ABUEMKEZE CHU <chu.amk22@gmail.com>
2026-08-03 16:38:25 +0100

    populating an array from user input

--- c-programming/arrays/main.c
+++ c-programming/arrays/main.c
@@ -1,39 +1,39 @@
-#include <stdio.h>
-
-int main(int argc, char const *argv[])
-{
-  int numbers[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
-  char grades[] = {'A', 'B', 'C', 'D'};
-
-  int size_arr = sizeof(numbers) / sizeof(numbers[0]);
-
-  for (size_t i = 0; i < size_arr; i++)
-  {
-    printf("%d\n", numbers[i]);
-  }
-
-  return 0;
-}
-
 // #include <stdio.h>
 
 // int main(int argc, char const *argv[])
 // {
-//   int scores[5];
+//   int numbers[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+//   char grades[] = {'A', 'B', 'C', 'D'};
 
-//   int size_of_arr = sizeof(scores) / sizeof(scores[0]);
+//   int size_arr = sizeof(numbers) / sizeof(numbers[0]);
 
-//   // for loop to keep asking the user until the array is filled.
-//   for (int count = 0; count < size_of_arr; count++)
+//   for (size_t i = 0; i < size_arr; i++)
 //   {
-//     printf("Enter number %d:", count+1);
-//     scanf("%d", &scores[count]);
-//   }
-
-//   for (size_t i = 0; i < size_of_arr; i++)
-//   {
-//     printf("%d ", scores[i]);
+//     printf("%d\n", numbers[i]);
 //   }
 
 //   return 0;
 // }
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+  int scores[5];
+
+  int size_of_arr = sizeof(scores) / sizeof(scores[0]);
+
+  // for loop to keep asking the user until the array is filled.
+  for (int count = 0; count < size_of_arr; count++)
+  {
+    printf("Enter number %d:", count+1);
+    scanf("%d", &scores[count]);
+  }
+
+  for (size_t i = 0; i < size_of_arr; i++)
+  {
+    printf("%d ", scores[i]);
+  }
+
+  return 0;
+}
