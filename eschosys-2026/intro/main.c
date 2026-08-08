723ae0832fda128e132d73694790deb1b7c5a2a4
Chu29 <chu.amk22@gmail.com>
2026-08-08 12:00:00 +0100

    intro: add hello world example

---
+++ eschosys-2026/intro/main.c
@@ -0,0 +1,66 @@
+// Header files.
+#include <stdio.h>
+
+// This piece of code displays "Hello World" on the console
+int main()
+{
+
+  printf("Hello World\n");
+
+  return 0;
+}
+
+/*
+- Reserved words in c
+
+1. auto
+2. double
+3. int
+4. struct
+5. break
+6. else
+7. long
+8. switch
+9. case
+10. enum
+11. register
+12. typedef
+13. char
+14. extern
+15. return
+16. union
+17. const
+18. float
+19. short
+20 unsigned
+21. continue
+22. for
+23. signed
+24. void
+25. default
+25. goto
+27. static
+28. size0f
+29. volatile
+30. do
+31. while
+32. if
+*/
+
+// 3. Identifiers
+int age; // var declaration
+age = 30; // var definition
+// int age = 30; // var definition
+int Age;
+
+// 4. Variables
+// <data_type> <variable_name> = <value>
+
+// 5. Literal/Constants
+#define PI 3.14
+//or
+const float pi = 3.14;
+
+// 6. Seperators: ; , {}, () []
+
+// 7. Format Specifiers
