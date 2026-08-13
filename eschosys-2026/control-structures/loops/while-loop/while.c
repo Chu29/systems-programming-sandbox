8c29adcdeda0a9258769ed883208029cf6281d1f
Chu29 <chu.amk22@gmail.com>
2026-08-13 12:00:00 +0100

    loops: add while-loop example

---
+++ eschosys-2026/control-structures/loops/while-loop/while.c
@@ -0,0 +1,27 @@
+/*
+var;
+while(condition){
+  // code
+
+  increment++;
+}
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  int count = 1;
+  // int sum = 0;
+
+  while (count < 2)
+  {
+    printf("%d\n", count);
+    count++;
+  }
+
+  return 0;
+}
+
+// displays numbers from 1-100
+// displays numbers from 1-100 in intervals of 2
